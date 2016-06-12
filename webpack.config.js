d78070d24faacc7745e7f1066a52260702690283
sifbuilder <sifbuilder@gmail.com>
2016-06-12 19:31:52 +0200

    plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var rucksack = require('rucksack-css')
 var webpack = require('webpack')
 var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   context: path.join(__dirname, './client'),
@@ -50,10 +51,15 @@ module.exports = {
     })
   ],
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor'),
+    new webpack.optimize.CommonsChunkPlugin('vendor.js'),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
-    })
+    }),
+		new HtmlWebpackPlugin({
+    title: 'd3rings',
+    template: 'html.ejs', // Load a custom template (ejs by default but can be changed) 
+    inject: 'body' // Inject all scripts into the body (this is the default so you can skip it) 
+  })
   ],
   devServer: {
     contentBase: './client',
